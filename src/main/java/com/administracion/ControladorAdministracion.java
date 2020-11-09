2989b63edfd295d3ef095eb54d30f9cb870bf3f0
Carlos Pena <cepr0190@gmail.com>
2020-11-09 13:28:31 -0300

    arreglo webserlevt paquete administracion

--- src/main/java/com/administracion/ControladorAdministracion.java
+++ src/main/java/com/administracion/ControladorAdministracion.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class ControladorInicio
  */
-@WebServlet("/ControladorInicio")
+@WebServlet("/ControladorAdministracion")
 public class ControladorAdministracion extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
