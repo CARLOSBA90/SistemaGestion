c913668f309b782301aa6577d33730b26cdf0356
Carlos Pena <cepr0190@gmail.com>
2020-11-24 12:48:39 -0300

    test modal pedidos

--- src/main/webapp/js/modalListarPedido.js
+++ src/main/webapp/js/modalListarPedido.js
@@ -5,10 +5,11 @@ $('#ListarModal').on('show.bs.modal', function(e) {
     
     arreglo = detalle.split(separador);
     
+    console.log(arreglo);
+    
     for(i=1;i<arreglo.length;i++){
     
     $(e.currentTarget).find('name="detalletabla"').val(arreglo[i]);
-    $(e.currentTarget).find('input[name="este2"]').val(arreglo[i]);
     }
     
     
