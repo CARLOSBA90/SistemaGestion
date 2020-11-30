933cc96a9dcf29ebe16dde97ec2e839ac97aa327
Carlos Pena <cepr0190@gmail.com>
2020-11-29 21:19:36 -0300

    bug fix: webServlet mal escrito en ControladorPDF

--- src/main/java/com/pdf/ControladorPDF.java
+++ src/main/java/com/pdf/ControladorPDF.java
@@ -24,7 +24,7 @@ import com.producto.Producto;
 /**
  * Servlet implementation class ControladorPedido
  */
-@WebServlet("/ControladorPedido")
+@WebServlet("/ControladorPDF")
 public class ControladorPDF extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
