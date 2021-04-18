ef1e0e8d08820424e524771a437ad095a7ca1159
CARLOSBA90 <cepr0190@gmail.com>
2021-04-18 20:19:28 -0300

    nuevo modo de acceso

--- src/main/java/com/conexion/DbManager.java
+++ src/main/java/com/conexion/DbManager.java
@@ -1,52 +1,51 @@
 package com.conexion;
-import java.io.IOException;  
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;  
 import java.sql.DriverManager;  
-import java.sql.SQLException;    
+import java.sql.SQLException;
+import java.util.Properties;    
 
 public class DbManager {
-
- //   public String date = new SimpleDateFormat("dd-MM-yyyy-HH-mm").format(new Date());
-    
-    private static final int LoginTimeout = 10;  
 
     public DbManager() {  
     }  
 
     public Connection createConnection() throws IOException, ClassNotFoundException, SQLException {  
-    	 /*   
-           Properties prop = new Properties();  
-      //  System.out.println("\n\n=======================\nJDBC Connector Test " + date);  
-      //  System.out.println("User home directory: " + System.getProperty("user.home"));  
-        String host;  
-        String username;  
-        String password;  
-       try {  
-        	
-            prop.load(new java.io.FileInputStream("C:\\Users/Kayreth/eclipse-workspace/Empresa_Calzados/files/mydb.cfg"));  
-            host = prop.getProperty("host").toString();  
-            username = prop.getProperty("username").toString();  
-            password = prop.getProperty("password").toString();  
-            driver = prop.getProperty("driver").toString();  
-        } catch (IOException e) {  
-            System.out.println("Unable to find mydb.cfg in " + System.getProperty("user.home") + "\n Please make sure that configuration file created in this folder.");  
-            
-            host = "Unknown HOST";  
-            username = "Unknown USER";  
-            password = "Unknown PASSWORD";  
-            driver = "Unknown DRIVER";  
-        }  
-
-      //  System.out.println("host: " + host + "\nusername: " + username + "\npassword: " + password + "\ndriver: " + driver);  
-*/
+    	
+    	InputStream inputStream =null;
+    	String url = "";
+    	String user = "";
+    	String pass = "";
+   	 
+		try {
+			Properties prop = new Properties();
+			String propFileName = "conexion.properties";
+			Class currentClass = new Object() { }.getClass().getEnclosingClass();
+ 
+			inputStream = currentClass.getClassLoader().getResourceAsStream(propFileName);
+ 
+			if (inputStream != null) {
+				prop.load(inputStream);
+			} else {
+				throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
+			}
+ 
+			// get the property value and print it out
+			url = prop.getProperty("url");
+			user = prop.getProperty("user");
+			pass = prop.getProperty("pass");
+ 
+		} catch (Exception e) {
+			System.out.println("Exception: " + e);
+		} finally {
+			inputStream.close();
+		}
+    	
         Class.forName("com.mysql.jdbc.Driver"); 
-        DriverManager.setLoginTimeout(LoginTimeout);  
-          Connection connection = DriverManager.getConnection("jdbc:mysql://us-cdbr-east-02.cleardb.com/heroku_a449933febac428", "b622585b18b608", "79f764f4"); 
-        
-     //  Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/empresa_calzado", "root", ""); 
-
-
-        return connection;  
+          Connection connection = DriverManager.getConnection(url,user,pass); 
+          return connection;  
     }  
 
 }
